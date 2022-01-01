e9584bb2d748dadfde4c95ee1f3e1062ba0b79b2
Marcelo Santos <rocha.mer21@gmail.com>
2022-01-01 16:04:25 -0300

    criado os controle e animacoes dos quatros eixos

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,6 +10,8 @@ export default class Game extends Phaser.Scene {
       frameWidth: 64,
       startFrame: 0,
     })
+
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   create() {
@@ -32,5 +34,52 @@ export default class Game extends Phaser.Scene {
 
     const tiles = map.addTilesetImage('tiles')
     const layer = map.createLayer(0, tiles, 0, 0)
+
+    this.player = this.add.sprite(400, 300, 'tiles', 54)
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('tiles', { start: 81, end: 83 }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('tiles', { start: 78, end: 80 }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('tiles', { start: 52, end: 54 }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('tiles', { start: 55, end: 57 }),
+      frameRate: 10,
+      repeat: -1,
+    })
+  }
+
+  update() {
+    if (!this.cursors) {
+      return
+    }
+    if (this.cursors.left?.isDown) {
+      this.player?.anims.play('left')
+    }
+    if (this.cursors.right?.isDown) {
+      this.player?.anims.play('right')
+    }
+    if (this.cursors.down?.isDown) {
+      this.player?.anims.play('down')
+    }
+    if (this.cursors.up?.isDown) {
+      this.player?.anims.play('up')
+    }
   }
 }
